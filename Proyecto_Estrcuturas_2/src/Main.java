64edb11ea31448925bd468e95e1be9570f0b02fd
JasonDeras <derasjason2@gmail.com>
2021-05-18 08:16:50 -0600

    Creando interfaz del proyecto

--- Proyecto_Estrcuturas_2/src/Main.java
+++ Proyecto_Estrcuturas_2/src/Main.java
@@ -26,7 +26,198 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jmb_Principal = new javax.swing.JMenuBar();
+        jm_Archivo = new javax.swing.JMenu();
+        jmi_Nuevo_Archivo = new javax.swing.JMenuItem();
+        jmi_Salvar_Archivo = new javax.swing.JMenuItem();
+        jmi_Cerrar_Archivo = new javax.swing.JMenuItem();
+        jmi_Cargar_Archivo = new javax.swing.JMenuItem();
+        jmi_Salir = new javax.swing.JMenuItem();
+        jmi_Campos = new javax.swing.JMenu();
+        jmi_Crear_Campo = new javax.swing.JMenuItem();
+        jmi_Modificar_Campo = new javax.swing.JMenuItem();
+        jmi_Borrar_Campo = new javax.swing.JMenuItem();
+        jmi_Listar_Campos = new javax.swing.JMenuItem();
+        jm_Registros = new javax.swing.JMenu();
+        jmi_Crear_Registro = new javax.swing.JMenuItem();
+        jmi_Borrar_Registro = new javax.swing.JMenuItem();
+        jmi_Buscar_Registro = new javax.swing.JMenuItem();
+        jmi_modreg = new javax.swing.JMenuItem();
+        jmi_cruzar = new javax.swing.JMenuItem();
+        jm_indices = new javax.swing.JMenu();
+        jmi_crearindices = new javax.swing.JMenuItem();
+        jmi_reindexar = new javax.swing.JMenuItem();
+        jm_Estandarizacion = new javax.swing.JMenu();
+        jmi_Exportar_Excel = new javax.swing.JMenuItem();
+        jmi_Exportrar_XML = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jm_Archivo.setText("Archivo");
+
+        jmi_Nuevo_Archivo.setText("Nuevo Archivo");
+        jmi_Nuevo_Archivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Nuevo_ArchivoActionPerformed(evt);
+            }
+        });
+        jm_Archivo.add(jmi_Nuevo_Archivo);
+
+        jmi_Salvar_Archivo.setText("Salvar Archivo");
+        jmi_Salvar_Archivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Salvar_ArchivoActionPerformed(evt);
+            }
+        });
+        jm_Archivo.add(jmi_Salvar_Archivo);
+
+        jmi_Cerrar_Archivo.setText("Cerrar Archivo");
+        jmi_Cerrar_Archivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Cerrar_ArchivoActionPerformed(evt);
+            }
+        });
+        jm_Archivo.add(jmi_Cerrar_Archivo);
+
+        jmi_Cargar_Archivo.setText("Cargar Archivo");
+        jmi_Cargar_Archivo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Cargar_ArchivoActionPerformed(evt);
+            }
+        });
+        jm_Archivo.add(jmi_Cargar_Archivo);
+
+        jmi_Salir.setText("Salir");
+        jmi_Salir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_SalirActionPerformed(evt);
+            }
+        });
+        jm_Archivo.add(jmi_Salir);
+
+        jmb_Principal.add(jm_Archivo);
+
+        jmi_Campos.setText("Campos");
+
+        jmi_Crear_Campo.setText("Crear Campo");
+        jmi_Crear_Campo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Crear_CampoActionPerformed(evt);
+            }
+        });
+        jmi_Campos.add(jmi_Crear_Campo);
+
+        jmi_Modificar_Campo.setText("Modificar Campo");
+        jmi_Modificar_Campo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Modificar_CampoActionPerformed(evt);
+            }
+        });
+        jmi_Campos.add(jmi_Modificar_Campo);
+
+        jmi_Borrar_Campo.setText("Borrar Campo");
+        jmi_Borrar_Campo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Borrar_CampoActionPerformed(evt);
+            }
+        });
+        jmi_Campos.add(jmi_Borrar_Campo);
+
+        jmi_Listar_Campos.setText("Listar Campos");
+        jmi_Listar_Campos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Listar_CamposActionPerformed(evt);
+            }
+        });
+        jmi_Campos.add(jmi_Listar_Campos);
+
+        jmb_Principal.add(jmi_Campos);
+
+        jm_Registros.setText("Registros");
+
+        jmi_Crear_Registro.setText("Crear Registro");
+        jmi_Crear_Registro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Crear_RegistroActionPerformed(evt);
+            }
+        });
+        jm_Registros.add(jmi_Crear_Registro);
+
+        jmi_Borrar_Registro.setText("Borrar Registro");
+        jmi_Borrar_Registro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Borrar_RegistroActionPerformed(evt);
+            }
+        });
+        jm_Registros.add(jmi_Borrar_Registro);
+
+        jmi_Buscar_Registro.setText("Buscar Registro");
+        jmi_Buscar_Registro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Buscar_RegistroActionPerformed(evt);
+            }
+        });
+        jm_Registros.add(jmi_Buscar_Registro);
+
+        jmi_modreg.setText("Modificar Registro");
+        jmi_modreg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_modregActionPerformed(evt);
+            }
+        });
+        jm_Registros.add(jmi_modreg);
+
+        jmi_cruzar.setText("Cruzar Archivos");
+        jmi_cruzar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_cruzarActionPerformed(evt);
+            }
+        });
+        jm_Registros.add(jmi_cruzar);
+
+        jmb_Principal.add(jm_Registros);
+
+        jm_indices.setText("Indice");
+
+        jmi_crearindices.setText("Crear Indices");
+        jmi_crearindices.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_crearindicesActionPerformed(evt);
+            }
+        });
+        jm_indices.add(jmi_crearindices);
+
+        jmi_reindexar.setText("Re Indexar Archivos");
+        jmi_reindexar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_reindexarActionPerformed(evt);
+            }
+        });
+        jm_indices.add(jmi_reindexar);
+
+        jmb_Principal.add(jm_indices);
+
+        jm_Estandarizacion.setText("Estandarizacion");
+
+        jmi_Exportar_Excel.setText("Exporat EXCEL");
+        jmi_Exportar_Excel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Exportar_ExcelActionPerformed(evt);
+            }
+        });
+        jm_Estandarizacion.add(jmi_Exportar_Excel);
+
+        jmi_Exportrar_XML.setText("Exportar XML");
+        jmi_Exportrar_XML.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_Exportrar_XMLActionPerformed(evt);
+            }
+        });
+        jm_Estandarizacion.add(jmi_Exportrar_XML);
+
+        jmb_Principal.add(jm_Estandarizacion);
+
+        setJMenuBar(jmb_Principal);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -36,11 +227,100 @@ public class Main extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 274, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jmi_Nuevo_ArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Nuevo_ArchivoActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Nuevo_ArchivoActionPerformed
+
+    private void jmi_Salvar_ArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Salvar_ArchivoActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Salvar_ArchivoActionPerformed
+
+    private void jmi_Cerrar_ArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Cerrar_ArchivoActionPerformed
+        // TODO add your handling code here:
+      
+    }//GEN-LAST:event_jmi_Cerrar_ArchivoActionPerformed
+
+    private void jmi_Cargar_ArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Cargar_ArchivoActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Cargar_ArchivoActionPerformed
+
+    private void jmi_SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_SalirActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jmi_SalirActionPerformed
+
+    private void jmi_Crear_CampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Crear_CampoActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Crear_CampoActionPerformed
+
+    private void jmi_Modificar_CampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Modificar_CampoActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Modificar_CampoActionPerformed
+
+    private void jmi_Borrar_CampoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Borrar_CampoActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jmi_Borrar_CampoActionPerformed
+
+    private void jmi_Listar_CamposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Listar_CamposActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Listar_CamposActionPerformed
+
+    private void jmi_Crear_RegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Crear_RegistroActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Crear_RegistroActionPerformed
+
+    private void jmi_Borrar_RegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Borrar_RegistroActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jmi_Borrar_RegistroActionPerformed
+
+    private void jmi_Buscar_RegistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Buscar_RegistroActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Buscar_RegistroActionPerformed
+
+    private void jmi_modregActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_modregActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_modregActionPerformed
+
+    private void jmi_cruzarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_cruzarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jmi_cruzarActionPerformed
+
+    private void jmi_crearindicesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_crearindicesActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jmi_crearindicesActionPerformed
+
+    private void jmi_reindexarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_reindexarActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jmi_reindexarActionPerformed
+
+    private void jmi_Exportar_ExcelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Exportar_ExcelActionPerformed
+        // TODO add your handling code here:
+       
+    }//GEN-LAST:event_jmi_Exportar_ExcelActionPerformed
+
+    private void jmi_Exportrar_XMLActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_Exportrar_XMLActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jmi_Exportrar_XMLActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +358,29 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jm_Archivo;
+    private javax.swing.JMenu jm_Estandarizacion;
+    private javax.swing.JMenu jm_Registros;
+    private javax.swing.JMenu jm_indices;
+    private javax.swing.JMenuBar jmb_Principal;
+    private javax.swing.JMenuItem jmi_Borrar_Campo;
+    private javax.swing.JMenuItem jmi_Borrar_Registro;
+    private javax.swing.JMenuItem jmi_Buscar_Registro;
+    private javax.swing.JMenu jmi_Campos;
+    private javax.swing.JMenuItem jmi_Cargar_Archivo;
+    private javax.swing.JMenuItem jmi_Cerrar_Archivo;
+    private javax.swing.JMenuItem jmi_Crear_Campo;
+    private javax.swing.JMenuItem jmi_Crear_Registro;
+    private javax.swing.JMenuItem jmi_Exportar_Excel;
+    private javax.swing.JMenuItem jmi_Exportrar_XML;
+    private javax.swing.JMenuItem jmi_Listar_Campos;
+    private javax.swing.JMenuItem jmi_Modificar_Campo;
+    private javax.swing.JMenuItem jmi_Nuevo_Archivo;
+    private javax.swing.JMenuItem jmi_Salir;
+    private javax.swing.JMenuItem jmi_Salvar_Archivo;
+    private javax.swing.JMenuItem jmi_crearindices;
+    private javax.swing.JMenuItem jmi_cruzar;
+    private javax.swing.JMenuItem jmi_modreg;
+    private javax.swing.JMenuItem jmi_reindexar;
     // End of variables declaration//GEN-END:variables
 }
